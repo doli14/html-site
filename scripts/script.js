16b2946bb08950d2b416c7c8217b71113058ea07
doli14 <154535051+doli14@users.noreply.github.com>
2023-12-21 18:49:03 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+var sasha= 'Отмените биологию и историю';
+sasha= prompt('Введите все данные вашей банковской карты')
+
+alert('Бонжур' + sasha);
+
+
+
+function buy()
+{
+document.getElementById('btn').style.background='green'
+document.getElementById('btn').innerHTML='В корзине'
+}
---
+++ scripts/themes.js
@@ -0,0 +1,26 @@
+const header = document.querySelector('.header')
+const main = document.querySelector('.main')
+const footer = document.querySelector('.footer')
+
+function dark()
+{
+    header.style.background = 'black';
+    header.style.transition = '1s' ;
+    main.style.background = 'black';
+    main.style.transition = '1s' ;
+    footer.style.background = 'black';
+    footer.style.transition = '1s' ;
+}
+const header = document.querySelector('.header')
+const main = document.querySelector('.main')
+const footer = document.querySelector('.footer')
+
+function light()
+{
+    header.style.background = 'white';
+    header.style.transition = '1s' ;
+    main.style.background = 'white';
+    main.style.transition = '1s' ;
+    footer.style.background = 'white';
+    footer.style.transition = '1s' ;
+}
